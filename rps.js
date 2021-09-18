34f9c4e7827dd429a138c9840a8f058d25584ef4
Tosin Akinlua <akinluarotimi@gmail.com>
2021-09-19 00:02:26 +0100

    Add event listener and animation to player and computer selection

--- rps.js
+++ rps.js
@@ -1,59 +1,102 @@
 let playerScore = 0;
 let computerScore = 0;
+let winner = '';
+let computerSelection = '';
+let playerSelection = '';
 
-function getHumanPlay() {
-    let humanPlay = '';
-    while (true) {
-        humanPlay = prompt("Please, enter one of rock, paper or scissors to play").toLowerCase();
-        if (humanPlay === 'rock' || humanPlay === 'paper' || humanPlay === 'scissors') {
-            break;
-        }
+const playerSelections = document.querySelectorAll('.player-selection');
+const computerSelections = document.querySelectorAll('.computer-selection');
+
+playerSelections.forEach(playRound);
+playerSelections.forEach(removeAnimation);
+computerSelections.forEach(removeAnimation);
+
+function playRound(e) {
+    e.addEventListener('click', () => {
+        playerSelection = e.id;
+        const CHOICES = Array.from(computerSelections);
+        let randomNumber = Math.floor(Math.random() * 3);
+        computerSelection = CHOICES[randomNumber].id;
+        displayRoundWinner(playerSelection, computerSelection);
+        animateSelection(e);
+        animateSelection(CHOICES[randomNumber]);
+    });
+}
+
+function animateSelection(selection) {
+    if (winner === 'tie') {
+        return selection.classList.add('selected', 'tie-black', 'tie-black:hover');
     }
-    return humanPlay;
+    else if (winner === selection.id) {
+       return selection.classList.add('selected', 'win-green', 'win-green:hover');
+    }
+    else if (winner !== selection.id) {
+        return selection.classList.add('selected', 'lose-red', 'lose-red:hover');
+    }
 }
 
-function getComputerPlay() {
-    const CHOICES = ['rock', 'paper', 'scissors'];
-    let randomNumber = Math.floor(Math.random() * 3);
-    return CHOICES[randomNumber];
+function removeAnimation(e) {
+    e.addEventListener('transitionend', (e) => {
+        if (e.propertyName !== 'transform') {
+            return;
+        }
+        e.target.classList.remove('selected', 'win-green', 'lose-red', 'tie-black');
+    });
 }
-
-function displaySelection (playerSelection, computerSelection) {
+/* function displaySelection (playerSelection, computerSelection) {
     return `Player: ${playerSelection}\nComputer: ${computerSelection}`;
-}
+} */
 
 function displayRoundWinner(player, computer) {
     let winningState;
 
     if (player === 'rock' && computer === 'scissors') {
         winningState = 'You win! Rock crushes scissors';
+        playerScore++;
+        winner = player;
     }
     else if (player === 'rock' && computer === 'paper') {
         winningState = 'You lose! Paper wraps rock';
+        computerScore++;
+        winner = computer;
     }
     else if (player === 'paper' && computer == 'scissors')
     {
         winningState = 'You lose! Scissors cuts paper';
+        computerScore++;
+        winner = computer;
     }
     else if (player === 'paper' && computer === 'rock') {
         winningState = 'You win! Paper wraps rock';
+        playerScore++;
+        winner = player;
     }
     else if (player === 'scissors' && computer === 'paper') {
         winningState = 'You win! Scissors cuts paper';
+        playerScore++;
+        winner = player;
     }
     else if (player === 'scissors' && computer === 'rock') {
         winningState = 'You lose! Rock crushes scissors';
+        computerScore++;
+        winner = computer;
     }
     else {
         winningState = 'It\'s a tie!';
+        playerScore += 0;
+        computerScore += 0;
+        winner = 'tie';
     }
 
     return winningState;
 }
 
-function getPlayerScore(winner) {
+/* function addWinnerStyles(selection) {
+    let playerSelection = getHumanPlay(e);
+    let computerSelection = getComputerPlay(e);
+    let winner = displayRoundWinner(playerSelection, computerSelection);
     if (winner.indexOf('You win!') !== -1) {
-        return ++playerScore;
+        return selection.classList.add('win-green');
     }
     return playerScore += 0;
 }
@@ -103,5 +146,4 @@ function playGame() {
         console.log('The game ends in a tie!');
     }
 }
-
-
+ */
