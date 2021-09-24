4d4cf908313d5f6bede53bc80c912b1da6fd6e4a
Tosin Akinlua <akinluarotimi@gmail.com>
2021-09-24 03:42:00 +0100

    Addfunctionality to display scores per round, final scores and game winner, and restart functionality

--- rps.js
+++ rps.js
@@ -4,25 +4,82 @@ let winner = '';
 let computerSelection = '';
 let playerSelection = '';
 
-const playerSelections = document.querySelectorAll('.player-selection');
+const WINNINGPOINTS = 5;
+const playerSelections = Array.from(document.querySelectorAll('.player-selection'));
 const computerSelections = document.querySelectorAll('.computer-selection');
+const humanScore = document.querySelector('.player-score');
+const aiScore = document.querySelector('.computer-score');
 
 playerSelections.forEach(playRound);
 playerSelections.forEach(removeAnimation);
 computerSelections.forEach(removeAnimation);
 
-function playRound(e) {
-    e.addEventListener('click', () => {
-        playerSelection = e.id;
+
+function playRound(element) {
+    element.addEventListener('click', () => {
         const CHOICES = Array.from(computerSelections);
         let randomNumber = Math.floor(Math.random() * 3);
+
+        playerSelection = element.id;
         computerSelection = CHOICES[randomNumber].id;
-        displayRoundWinner(playerSelection, computerSelection);
-        animateSelection(e);
+
+        getRoundWinner(playerSelection, computerSelection);
+        humanScore.textContent = playerScore;
+        aiScore.textContent = computerScore;
+        animateSelection(element);
         animateSelection(CHOICES[randomNumber]);
+
+        if (playerScore === WINNINGPOINTS || computerScore === WINNINGPOINTS) {
+            displayGameWinner();
+            disableSelection(playerSelections);
+        }
+    });
+    restart();
+}
+
+function restart() {
+    const popupBox = document.querySelector('.popup-box');
+    const popupBoxBtn = document.querySelector('.popup-box button');
+    popupBoxBtn.addEventListener('click', () => {
+        playerScore = 0;
+        computerScore = 0;
+        humanScore.textContent = 0;
+        aiScore.textContent = 0;
+        popupBox.style.display = 'none';
+        popupBox.classList.remove('popup-box-win', 'popup-box-lose');
+        enableSelection(playerSelections);
     });
 }
 
+function disableSelection(selections) {
+    selections.forEach((selection) => {
+        selection.disabled = true;
+        selection.classList.add('disable-click', 'disable-click:hover');
+    });
+}
+
+function enableSelection(selections) {
+    selections.forEach((selection) => {
+        selection.disabled = false;
+        selection.classList.remove('disable-click', 'disable-click:hover');
+    });
+}
+
+function displayGameWinner() {
+    const popupBox = document.querySelector('.popup-box');
+    let popupMsg = document.querySelector('.popup-box p');
+    popupBox.style.display = 'initial';
+
+    if (playerScore > computerScore) {
+        popupMsg.textContent = `Great Job! You won the game ${playerScore} - ${computerScore}, congratulations!`;
+        popupBox.classList.add('popup-box-win');    
+    }
+    else {
+        popupMsg.textContent = `Sorry! You lost the game ${playerScore} - ${computerScore}. Better luck next time.`;
+        popupBox.classList.add('popup-box-lose');
+    }    
+}
+        
 function animateSelection(selection) {
     if (winner === 'tie') {
         return selection.classList.add('selected', 'tie-black', 'tie-black:hover');
@@ -30,9 +87,7 @@ function animateSelection(selection) {
     else if (winner === selection.id) {
        return selection.classList.add('selected', 'win-green', 'win-green:hover');
     }
-    else if (winner !== selection.id) {
-        return selection.classList.add('selected', 'lose-red', 'lose-red:hover');
-    }
+    return selection.classList.add('selected', 'lose-red', 'lose-red:hover');
 }
 
 function removeAnimation(e) {
@@ -43,11 +98,8 @@ function removeAnimation(e) {
         e.target.classList.remove('selected', 'win-green', 'lose-red', 'tie-black');
     });
 }
-/* function displaySelection (playerSelection, computerSelection) {
-    return `Player: ${playerSelection}\nComputer: ${computerSelection}`;
-} */
 
-function displayRoundWinner(player, computer) {
+function getRoundWinner(player, computer) {
     let winningState;
 
     if (player === 'rock' && computer === 'scissors') {
@@ -91,59 +143,3 @@ function displayRoundWinner(player, computer) {
     return winningState;
 }
 
-/* function addWinnerStyles(selection) {
-    let playerSelection = getHumanPlay(e);
-    let computerSelection = getComputerPlay(e);
-    let winner = displayRoundWinner(playerSelection, computerSelection);
-    if (winner.indexOf('You win!') !== -1) {
-        return selection.classList.add('win-green');
-    }
-    return playerScore += 0;
-}
-
-function getComputerScore(winner) {
-    if (winner.indexOf('You lose!') !== -1) {
-        return ++computerScore;
-    }
-    return computerScore += 0;
-}
-
-function displayScore(playerScore, computerScore) {
-    return `PlayerScore: ${playerScore} ComputerScore: ${computerScore}`;
-}
-
-// Plays one round of game
-function playRound() {
-    let playerSelection = getHumanPlay();
-    let computerSelection = getComputerPlay();
-    let roundWinner = displayRoundWinner(playerSelection, computerSelection);
-    playerScore = getPlayerScore(roundWinner);
-    computerScore = getComputerScore(roundWinner);
-    let selection = displaySelection(playerSelection, computerSelection);
-    let scoreBoard = displayScore(playerScore, computerScore);
-    
-    return `${selection}\n${roundWinner}\n${scoreBoard}`;
-}
-
-// Plays 5 rounds of game or break when one player gets to 3
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound());
-        if (playerScore === 3 || computerScore === 3) {
-            break;
-        }
-    }
-    
-    console.log(`Final Score - Player: ${playerScore} Computer: ${computerScore}`);
-        
-    if (playerScore > computerScore) {
-        console.log('Congratulations! You won the game!');
-    }
-    else if (playerScore < computerScore) {
-        console.log('Sorry, you lost. Better luck next time!');
-    }
-    else {
-        console.log('The game ends in a tie!');
-    }
-}
- */
